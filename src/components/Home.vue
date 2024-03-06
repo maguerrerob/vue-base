30b8153059b9f6e92fa23f752aff76b7033444ba
Manuel Alejandro <manuel.guerrero.boza.al@iespoligonosur.org>
2024-03-06 23:17:32 +0000

    falta poco

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <Carrusel></Carrusel>
   <h1 class="mt-5">Top Rated</h1>
     <div class="row">
-      <div class="col-3" v-for="movie in moviesFiltradas" :key="movie.id">
+      <div class="col-3" v-for="movie in movies" :key="movie.id">
         <div class="card mb-3">
           <a href="" class="card-img-top ms-0 img-fluid"><img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" class="col-3 card-img-top img-fluid"></a>
           <span class="position-absolute top-0 start-100 translate-middle d-inline-block rounded-circle bg-success text-white p-1" id="point">
@@ -14,6 +14,9 @@
             <p class="card-text">Fecha de salida: <span class="text-success">{{ movie.release_date }}</span></p>
             <button type="button" @click="addWatchlist(movie.id)" class="btn btn-outline-primary" id="mybutton">Añadir a watchlist</button>
             <router-link :to="{ name: 'Movie', params: { id: movie.id }}" class="btn btn-primary">Detalles</router-link>
+            <span class="ms-5" v-if="movie.vote_average > 8.55">
+              🌟
+            </span>
           </div>
         </div>
       </div>
@@ -21,7 +24,7 @@
 </template>
 
 <script setup>
-  import { ref, onMounted, computed, watch } from 'vue'
+  import { ref, onMounted, computed } from 'vue'
   import Carrusel from "./Carrusel.vue"
 
   const bearerToken = ref('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWRlMzI4MzZhYTIxNzIyMjk1OTcxMGFhNGJmYTY1NiIsInN1YiI6IjY1YTkxYmJjNTVjMWY0MDEyODg5ZWE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I7FRPNfYWFgq6giDmM43GaiYUaLSYyLM-6m7kJywMd0')
@@ -43,9 +46,9 @@
     getMoviesTopRated();
   })
 
-  const moviesFiltradas = computed(() => {
-    return movies.value.filter(movie => movie.vote_average > 8.55)
-  })
+  // const moviesFiltradas = computed(() => {
+  //   return movies.value.filter(movie => movie.vote_average > 8.55)
+  // })
 
   function addWatchlist(movieId){
         fetch(`https://api.themoviedb.org/3/account/20931000/watchlist`, {
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,112 +1,166 @@
 <template>
-  <p>{{ $route.params.id }}</p>
-  <div v-if="movie" class="container">
-    <h1>Detalles de película {{ movie.title }}</h1>
+
+  <div class="row">
+    <div class="mx-auto mb-3" v-if="movie">
+      <div class="card mb-12">
+        <div class="row">
+          <div class="col-4">
+            <img :src="'https://image.tmdb.org/t/p/w1280' + movie.poster_path" id="imgMovie" class="col-12 card-img-top img-fluid" alt="imgMovie">
+          </div>
+          <div class="col-8">
+            <h4 class="card-tittle mt-3">{{ movie.title }}</h4>
+            <p class="card-text">{{ movie.overview }}</p>
+            <p class="card-text">Fecha de salida: <span class="text-success">{{ movie.release_date }}</span></p>
+            <div v-if="providersmovie.rent">
+              <h5>Alquiler</h5>
+              <div v-for="provider in providersmovie.rent" :key="provider.id" class="d-inline-block mx-2">
+                <img :src="'https://image.tmdb.org/t/p/w1280' + provider.logo_path" class="img-thumbnail" alt="alquiler" id="providersRent">
+              </div>
+            </div>
+            <div v-if="providersmovie.buy">
+              <h5 class="mt-3">Compra</h5>
+              <div v-for="providere in providersmovie.buy" :key="providere.id" class="d-inline-block mx-2">
+                <img :src="'https://image.tmdb.org/t/p/w1280' + providere.logo_path" class="img-thumbnail" alt="compra" id="providersRent">
+              </div>
+            </div>
+            <div class="mt-auto">
+              <button type="button" @click="addWatchlist(movie.id)" class="btn btn-outline-primary mt-3" id="mybutton">Añadir a watchlist</button>
+            </div>
+            <div class="ventanaModal ml-2 mt-3">
+              <button @click="openModal" class="btn btn-outline-primary">Trailer</button>
+              
+              <!-- Modal HTML -->
+              <div class="modal fade" v-if="modalVisible">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" @click="closeModal" aria-hidden="true">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                      <iframe id="four-seasons-video" width="100%" height="315" :src="trailerUrl" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- <p id="genero">Género</p>
+            <ul>
+                <li v-for="genreId in movie.genre_ids" :key="genreId">{{ genres.find(genre => genre.id === genreId).name }}</li>
+            </ul> -->
+          </div>
+        </div>
+        <span class="position-absolute top-0 start-100 translate-middle d-inline-block rounded-circle bg-success text-white p-1">
+          {{ (movie.vote_average).toFixed(2) }}
+          <span class="visually-hidden">unread messages</span>
+        </span>
+      </div>
+    </div>
   </div>
+
+  <div class="container mx-5">
+    <h3>REPARTO</h3>
+
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+      <div v-if="reviewsmovie">
+        <li class="nav-item" role="presentation">
+          <button class="nav-link active" id="reviews" data-bs-toggle="tab" data-bs-target="#review-pane" type="button" role="tab" aria-controls="review-pane" aria-selected="true">Home</button>
+        </li>
+      </div>
+        
+      <li class="nav-item" role="presentation">
+        <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Profile</button>
+      </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+      <div class="tab-pane fade show active" id="review-pane" role="tabpanel" aria-labelledby="review" tabindex="0">
+        <div v-for="review in reviewsmovie.results" :key="review.id" >
+          <p>{{ review.author }}</p>
+        </div>
+        <p>jjj</p>
+      </div>
+      <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
+    </div>
+  </div>
+    
+
   
 </template>
 
-<script>
+<script setup>
   import { ref, onMounted, onUpdated } from 'vue';
   import { useRoute } from "vue-router";
 
-  const bearerToken = ref('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWRlMzI4MzZhYTIxNzIyMjk1OTcxMGFhNGJmYTY1NiIsInN1YiI6IjY1YTkxYmJjNTVjMWY0MDEyODg5ZWE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I7FRPNfYWFgq6giDmM43GaiYUaLSYyLM-6m7kJywMd0')
-  
+  const bearerToken = ref('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWRlMzI4MzZhYTIxNzIyMjk1OTcxMGFhNGJmYTY1NiIsInN1YiI6IjY1YTkxYmJjNTVjMWY0MDEyODg5ZWE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I7FRPNfYWFgq6giDmM43GaiYUaLSYyLM-6m7kJywMd0')  
   const apikey = ref("41de32836aa217222959710aa4bfa656")
   
-  const $route = useRoute();
+  const route = useRoute();
   const movie = ref();
   const providersmovie = ref([]);
   const castmovie = ref([]);
   const reviewsmovie = ref([]);
   const recommendationsmovie = ref([]);
+  const genres = ref([]);
   const trailerUrl = ref('');
+  const modalVisible = ref(false);
+  
 
-  onMounted(async () => {
+  const getMovieDetails = async () => {
     try {
-      const options = {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${route.params.id}?api_key=${apikey}&language=es&page=1&append_to_response=watch/providers,credits,reviews,videos,recommendations`, {
         method: 'GET',
         headers: {
           accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWRlMzI4MzZhYTIxNzIyMjk1OTcxMGFhNGJmYTY1NiIsInN1YiI6IjY1YTkxYmJjNTVjMWY0MDEyODg5ZWE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I7FRPNfYWFgq6giDmM43GaiYUaLSYyLM-6m7kJywMd0'
+          Authorization: 'Bearer ' + bearerToken.value
         }
-      };
-
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/${$route.params.id}?api_key=41de32836aa217222959710aa4bfa656&append_to_response=watch/providers,credits,reviews,videos,recommendations`,
-        
-        options
-      );
+      });
 
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
 
-      const data = await response.json();
+      const data = await response.json()
       movie.value = data;
       providersmovie.value = data['watch/providers'].results.ES;
-      castmovie.value = data.credits.cast
-      reviewsmovie.value = data.reviews.results
-      recommendationsmovie.value = data.recommendations.results
+      castmovie.value = data.credits.cast;
+      reviewsmovie.value = data.reviews.results;
+      recommendationsmovie.value = data.recommendations;
       
       
       const trailerVideo = data.videos.results.find(video => video.type === "Trailer");
           if (trailerVideo) {
               const keytrailer = trailerVideo.key;
               trailerUrl.value = `https://www.youtube.com/embed/${keytrailer}`;
-              console.log(trailerUrl.value)
-
           } else {
               console.error("No se encontró el tráiler de la película.");
           }
 
     } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
+      console.error('Ocurrió un error en la solicitud', error);
     }
-});
+  };
 
-  onUpdated(async () => {
-    try {
-      const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWRlMzI4MzZhYTIxNzIyMjk1OTcxMGFhNGJmYTY1NiIsInN1YiI6IjY1YTkxYmJjNTVjMWY0MDEyODg5ZWE1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.I7FRPNfYWFgq6giDmM43GaiYUaLSYyLM-6m7kJywMd0'
-        }
-      };
-
-      const response = await fetch(
-        `https://api.themoviedb.org/3/movie/${$route.params.id}?api_key=41de32836aa217222959710aa4bfa656&append_to_response=watch/providers,credits,reviews,videos,recommendations`,
-        
-        options
-      );
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+  const getGenres = () => {
+    console.log(genres.value)
+    fetch('https://api.themoviedb.org/3/genre/movie/list?language=es-US', {
+      headers: {
+        'accept': 'application/json',
+        'Authorization': 'Bearer ' + bearerToken.value
       }
+    })
+    .then(response => response.json())
+    .then(data => {
+      genres.value = data.genres;
+    })
+  };
 
-      const data = await response.json();
-      movie.value = data;
-      providersmovie.value = data['watch/providers'].results.ES;
-      castmovie.value = data.credits.cast
-      reviewsmovie.value = data.reviews.results
-      recommendationsmovie.value = data.recommendations.results
-      
-      
-      const trailerVideo = data.videos.results.find(video => video.type === "Trailer");
-          if (trailerVideo) {
-              const keytrailer = trailerVideo.key;
-              trailerUrl.value = `https://www.youtube.com/embed/${keytrailer}`;
-              console.log(trailerUrl.value)
+  onMounted(() => {
+    getMovieDetails()
+    getGenres()
+  })
 
-          } else {
-              console.error("No se encontró el tráiler de la película.");
-          }
-
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
-    }
-  });
+  onUpdated(() => {
+    getMovieDetails()
+  })
 
   function addWatchlist(){
     fetch(`https://api.themoviedb.org/3/account/20931000/watchlist`, {
@@ -126,14 +180,30 @@
     .catch(err => console.error(err));
   }
 
-  const stopVideo = () => {
-    const iframe = document.querySelector('iframe');
-    const iframeSrc = iframe.src;
-    iframe.src = iframeSrc;
-  }
+  const openModal = () => {
+    modalVisible.value = true;
+  };
+
+  const closeModal = () => {
+    modalVisible.value = false;
+    // Detener la reproducción del video al cerrar la modal
+    trailerUrl.value = "";
+    // Restablecer la URL del video cuando se cierra la modal
+    nextTick(() => {
+      trailerUrl.value = "https://www.youtube.com/embed/GRxofEmo3HA";
+    });
+  };
+  
+  
 
 </script>
 
 <style>
+  #imgMovie{
+    height: 800px;
+  }
 
+  #providersRent{
+    width: 70px;
+  }
 </style>
